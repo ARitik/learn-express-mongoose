ca03554102e720213fca06f7aeb7a2213b08c0b3
bruvgox <ambadiritik@gmail.com>
2024-03-15 17:32:49 -0400

    Finished class activities

--- models/author.js
+++ models/author.js
@@ -4,19 +4,16 @@ var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
   {
-    first_name: {type: String, required: true, maxLength: 100},
-    family_name: {type: String, required: true, maxLength: 100},
+    first_name: {type: String, required: true, maxLength: 100}, 
+    family_name: {type: String, required: true, maxLength: 100}, 
     date_of_birth: {type: Date},
     date_of_death: {type: Date},
   }
 );
 
-// Virtual for author's full name
 AuthorSchema
 .virtual('name')
-.get(function () {
-// To avoid errors in cases where an author does not have either a family name or first name
-// We want to make sure we handle the exception by returning an empty string for that case
+.get(function () { 
   var fullname = '';
   if (this.first_name && this.family_name) {
     fullname = this.family_name + ', ' + this.first_name
@@ -27,8 +24,14 @@ AuthorSchema
   return fullname;
 });
 
-// Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {});
+AuthorSchema
+.virtual('lifespan')
+.get(function() {
+  var lifespan = '';
+  if (this.date_of_birth && this.date_of_death) {
+    lifespan = this.date_of_birth.getFullYear().toString() + ' - ' + this.date_of_death.getFullYear().toString();
+  }
+  return lifespan;
+});
 
-//Export model
 module.exports = mongoose.model('Author', AuthorSchema);
